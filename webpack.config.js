d28716e89d781b75745008383239006ef4cbf724
Catalin Popescu <catalinp86@gmail.com>
2022-02-09 13:26:34 -0500

    refactored renderer locations

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  resolve: {
+    modules: [
+      path.resolve('./lib'),
+      path.resolve('./node_modules')
+    ]
+  },
   mode: 'development',
-  entry: ['./lib/components/Index.js'],
+  entry: ['./lib/renderers/dom.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
